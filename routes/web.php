511e1ec44c9f1e2b2cea1ee22df095e28c254398
andy <andy@31interactive.com>
2018-10-15 23:47:41 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/broadcast-test', function() {
+	\App\Events\ExampleEvent::dispatch('The server time is now ' . date('H:i:s'));
+});
